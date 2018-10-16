1876bf6b4034e37bf807763fb27142e2da3dfcb1
laithalrousan <laith.rosan98@gmail.com>
2018-10-16 20:02:34 +0300

    projeect

---
+++ warmip_1/monkey_trouble.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+def monkey_trouble(a_smile, b_smile):
+  if (a_smile and b_smile) or (not a_smile and not b_smile) :
+   return True  
+  else :  
+   return False
+
